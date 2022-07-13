e8ca393874c2d09905ec6a9dd86c9d6897f7e78b
Adeayo Adewumi <80199070+adeayopy@users.noreply.github.com>
2022-07-13 09:28:22 +0100

    Challenge - 1

--- src/assignment.js
+++ src/assignment.js
@@ -24,13 +24,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * Count all even numbers within an array of numbers.
  * If you did Challenge - 2, remove the comment in the line just after this function
  * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
+ * @param {Array} arrayOfNumbers the array of numbers to sum
+ * @returns {number} the sum of the numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+ function sumOfNumbers(arrayOfNumbers) {
+    let sumArray = 0;
+    for (let num of arrayOfNumbers) {
+        sumArray = sumArray + num
+        return sumArray;
+    }
+
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 3
